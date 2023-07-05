3c839a5a54f7a539ebe56e83b97b699ba01f1a9e
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-07-05 12:43:21 +0100

    Updated Two Sum

--- Two Sum.py
+++ Two Sum.py
@@ -1,6 +1,6 @@
 def twoSum(nums, target):
-    # O(nlogn) - time
-    # O(n) - space
+    # O(n) - time
+    # O(len(d)) - space
     if len(nums) == 2:
         return [0, 1]
     d = dict()
@@ -9,6 +9,30 @@ def twoSum(nums, target):
             d[nums[i]].append(i)
         else:
             d[nums[i]] = [i]
+    for key in d:
+        if key == (target - key) and len(d[key]) > 1:
+            return d[key][0:2]
+        if target - key in d and key != (target - key):
+            res = d[key] + d[target - key]
+            res.sort()
+            return res
+    return False
+
+
+assert twoSum([3,2,3], 6) == [0, 2]
+assert twoSum([3,3,3], 6) == [0, 1]
+assert twoSum([3,2], 5) == [0, 1]
+assert twoSum([2,11,15,7], 9) == [0, 3]
+assert twoSum([3,2,4], 6) == [1,2]
+assert twoSum([3,2,3,1,2,3,4,5,7], 1) == False
+
+
+def twoSum_(nums, target):
+    # O(nlogn) - time
+    # O(n) - space
+    if len(nums) == 2:
+        return [0, 1]
+    d = dict()
     nums.sort()
     left = 0
     right = len(nums)-1
@@ -24,14 +48,3 @@ def twoSum(nums, target):
             right -= 1
         else:
             left += 1
-
-
-assert twoSum([3,2,3], 6) == [0, 2]
-assert twoSum([3,3,3], 6) == [0, 1]
-assert twoSum([3,2], 5) == [0, 1]
-assert twoSum([2,11,15,7], 9) == [0, 3]
-assert twoSum([3,2,4], 6) == [1,2]
-assert twoSum([3,2,3,1,2,3,4,5,7], 1) == False
-
-
-
