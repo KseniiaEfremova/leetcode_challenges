1e859c0529e675870c10ab61c5b70907bbd9eed9
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-07-14 14:24:42 +0100

    Refactored

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -1,22 +1,22 @@
 def isPalindrome(x: int) -> bool:
-    # O(1) time
-    # O(n) space
+    # O(len(digits)) time
+    # O(len(digits)) space
     if x < 0:
         return False
-    digits = []
+    first_num = x
+    new_num = 0
     while x > 0:
-        digits.append(x % 10)
+        new_num = new_num*10 + x % 10
         x = x // 10
-    return digits == digits[::-1]
+    return first_num == new_num
 
 
-
-assert isPalindrome(121) == True
-assert isPalindrome(11) == True
-assert isPalindrome(1) == True
-assert isPalindrome(-1) == False
-assert isPalindrome(11121) == False
-assert isPalindrome(1212) == False
+assert isPalindrome(121) is True
+assert isPalindrome(1001) is True
+assert isPalindrome(1) is True
+assert isPalindrome(-1) is False
+assert isPalindrome(11121) is False
+assert isPalindrome(1212) is False
 
 
 # def isPalindrome(x: int) -> bool:
