be8e46e738d5567b59933c8c42538d37c83c73b7
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-07-17 11:52:12 +0100

    Refactored

--- Merge Two Sorted Lists.py
+++ Merge Two Sorted Lists.py
@@ -1,54 +1,57 @@
 class Node:
-    def __init__(self, val=0, next_element=None):
-        self.next_element = next_element
+    def __init__(self, val=0, next=None):
+        self.next = next
         self.val = val
 
     def print_values(self):
-        print(self.val, end="")
-        node = self.next_element
+        print(self.val, end="-")
+        node = self.next
         while node is not None:
-            print(node.val, end="")
-            node = node.next_element
+            print(node.val, end="-")
+            node = node.next
 
 
 l_1 = Node(1)
-l_2 = Node(2)
-l_4 = Node(4)
+l_3 = Node(3)
+l_5 = Node(5)
+l_9 = Node(9)
 
-r_3 = Node(6)
-r_6 = Node(7)
-r_8 = Node(8)
+r_2 = Node(2)
+r_10 = Node(10)
 
-l_1.next_element = l_2
-l_2.next_element = l_4
+l_1.next = l_3
+l_3.next = l_5
+l_5.next = l_9
 
-r_3.next_element = r_6
-r_6.next_element = r_8
-
-
-def create_linked_list(n):  # n - number of nodes
-    if n == 0:
-        return None
-    linked_list = Node(10)
-    linked_list.next_element = create_linked_list(n - 1)
-    return linked_list
+r_2.next = r_10
 
 
-def mergeTwoLists(list1, list2):
-    if list1 is None:
-        return list2
-    if list2 is None:
-        return list1
-    if list1 is None and list2 is None:
-        return None
+# def mergeTwoLists(list1, list2):
+#     if list1 is None:
+#         return list2
+#     if list2 is None:
+#         return list1
+#     if list1 is None and list2 is None:
+#         return None
+#
+#     if list1.val < list2.val:
+#         node = list1
+#         node.next = mergeTwoLists(list1.next, list2)
+#     else:
+#         node = list2
+#         node.next = mergeTwoLists(list1, list2.next)
+#     return node
 
-    if list1.val < list2.val:
-        node = list1
-        node.next_element = mergeTwoLists(list1.next_element, list2)
-    else:
-        node = list2
-        node.next_element = mergeTwoLists(list1, list2.next_element)
-    return node
 
-
-mergeTwoLists(l_1, r_3).print_values()
+def mergeTwoLists2(list1, list2):
+    head = new_list = Node()
+    while list1 and list2:
+        if list1.val < list2.val:
+            new_list.next = list1
+            list1 = list1.next
+        else:
+            new_list.next = list2
+            list2 = list2.next
+        new_list = new_list.next
+    new_list.next = list1 or list2
+    return head.next
