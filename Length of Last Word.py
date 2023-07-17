42f08b5ff695fb9c63fc92603f7b0b4150c4e13a
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-07-17 11:20:34 +0100

    Refactored

--- Length of Last Word.py
+++ Length of Last Word.py
@@ -1,3 +1,15 @@
 def lengthOfLastWord(s: str) -> int:
-    list_words = s.split()
-    return len(list_words[-1])
+    count = 0
+    for k in range(len(s)-1, -1, -1):
+        if not s[k].isspace():
+            count += 1
+        elif count:
+            return count
+    return count
+
+
+assert lengthOfLastWord("qq qq") == 2
+assert lengthOfLastWord("qq qq  ") == 2
+assert lengthOfLastWord("qqqq") == 4
+assert lengthOfLastWord("qq qq") == 2
+assert lengthOfLastWord("qq qq") == 2
