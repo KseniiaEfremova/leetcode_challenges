0c37ce75ef80f1d3b00bd56dce86bf650d04ff8b
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-07-19 12:56:41 +0100

    Solved Add Binary

--- Add Binary.py
+++ Add Binary.py
@@ -1,28 +1,15 @@
 def addBinary(a: str, b: str) -> str:
-    a = [int(n) for n in list(a)]
-    b = [int(n) for n in list(b)]
-    sum = []
-    rest = 0
-    number = 0
-    k = -1
-    while k > -len(a):
-        number = a[k] + b[k]
-        if number == 2:
-            if rest == 0:
-                sum.append(0)
-                rest = 1
-            else:
-                sum.append(1)
-                rest = 0
-        else:
-            sum.append(number)
-        if k - 1 == -len(b) - 1:
-            if rest == 1:
-                sum.append()
-                sum.append(a[:k:-1])
-        k -= 1
-
-
-
-    # if rest == 1:
-    #     sum +
+    res = []
+    n = max(len(a), len(b))
+    carry = 0
+    for i in range(n):
+        ia = len(a) - 1 - i
+        ib = len(b) - 1 - i
+        ca = int(a[ia]) if ia >= 0 else 0
+        cb = int(b[ib]) if ib >= 0 else 0
+        t = carry + ca + cb
+        res.append(t % 2)
+        carry = t // 2
+    if carry > 0:
+        res.append(carry)
+    return ''.join(str(res[i]) for i in range(len(res) - 1, -1, -1))
