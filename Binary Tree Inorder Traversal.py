c0eddb572884a6846a8d2c6c3d3f71b73efa5678
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-07-24 10:36:31 +0100

    Refactored

--- Binary Tree Inorder Traversal.py
+++ Binary Tree Inorder Traversal.py
@@ -1,28 +1,38 @@
+from typing import List, Optional
+
 # Definition for a binary tree node.
-class Node:
+class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
 
-def inorderTraversal(root) -> list:
+def inorderTraversal(root: Optional[TreeNode]) -> List[int]:
+    result = []
+    inorderTraversalRec(root, result)
+    return result
+
+
+def inorderTraversalRec(root: Optional[TreeNode], result: List[int]):
     if root is None:
-        return []
-    return inorderTraversal(root.left) + [root.val] + inorderTraversal(root.right)
+        return
+    inorderTraversalRec(root.left, result)
+    result.append(root.val)
+    inorderTraversalRec(root.right, result)
 
 
-node1 = Node(1)
-node2 = Node(2)
-node3 = Node(3)
-node4 = Node(4)
-node5 = Node(5)
-node6 = Node(6)
-node7 = Node(7)
+node1 = TreeNode(1)
+node2 = TreeNode(2)
+node3 = TreeNode(3)
+node4 = TreeNode(4)
+node5 = TreeNode(5)
+node6 = TreeNode(6)
+node7 = TreeNode(7)
 
 node4.left = node2
 node4.right = node5
 node2.left = node1
 node2.right = node3
-node5.right = node6
-node6.left = node7
+node5.left = node6
+node5.right = node7
