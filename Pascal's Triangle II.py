41d544a8d96abadaa65a4cd8e147c742aff90ec7
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-08-14 14:20:24 +0100

    Solved Pascal's Triangle II

---
+++ Pascal's Triangle II.py
@@ -0,0 +1,12 @@
+def getRow(rowIndex: int):
+    # O(n**2) time
+    # O(n) spice
+    res = [1]
+    for i in range(rowIndex):
+        temp = [0] + res + [0]
+        new_row = []
+        for k in range(len(temp)-1):
+            new_row.append(temp[k]+temp[k+1])
+        res = new_row
+
+    return res
